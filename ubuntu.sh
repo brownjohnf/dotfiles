2389829e20bcbda9a0470e5205e229dfc58b02f1
John (Jack) Brown <jack@brownjohnf.com>
2015-10-24 22:42:23 -0700

    Silence some of the stdout in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,7 +9,7 @@ HERE="$HOME/dotfiles"
 binpath=$HOME/.bin
 
 echo '--> Installing dependencies...'
-sudo apt-get update -y && sudo apt-get install -y aptitude
+sudo apt-get update -y > /dev/null && sudo apt-get install -y aptitude > /dev/null
 
 # Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
@@ -20,7 +20,7 @@ echo deb http://downloads.hipchat.com/linux/apt stable main | sudo tee /etc/apt/
 wget -O - https://www.hipchat.com/keys/hipchat-linux.key | sudo apt-key add -
 
 # Go back and install everything
-sudo aptitude update && sudo aptitude install -y \
+sudo aptitude update > /dev/null  && sudo aptitude install -y \
   vim curl htop wget screen git alsa-base unzip \
   alsa-tools alsa-utils build-essential leafpad \
   chromium-browser cryptsetup pcmanfm dkms ecryptfs-utils \
@@ -28,12 +28,13 @@ sudo aptitude update && sudo aptitude install -y \
   nmap whois network-manager-openvpn nfs-common \
   i3 i3-wm i3lock i3status xinit x11-xserver-utils xfce4-terminal \
   python-numpy python-opengl openscad python-wxgtk2.8 zsh tmux \
-  ipcalc spotify-client hipchat
+  ipcalc spotify-client hipchat \
+  > /dev/null
 
 # Sublime & Cura
 curl "http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb" > /tmp/sublime.deb
 curl "software.ultimaker.com/current/cura_15.04-debian_amd64.deb" > /tmp/cura.deb
-sudo dpkg -i /tmp/sublime.deb /tmp/cura.deb
+sudo dpkg -i /tmp/sublime.deb /tmp/cura.deb > /dev/null
 
 # Docker
 wget -qO- https://get.docker.com/ | sh
